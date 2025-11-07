25cd87c1e16c4428830e0417bd3a85b3c09c5cf8
Pedro Brito <pedroab0119@gmail.com>
2025-11-07 10:05:10 -0300

    update mp3 para testes

--- src/App.jsx
+++ src/App.jsx
@@ -21,10 +21,10 @@ export default function TrackMixer() {
   ];
 
   const tracks = [
-    // { id: 1, name: "Ambient Pad", base: "AMBIENTE PAD" },
-    // { id: 2, name: "Shimmer Pad", base: "SHIMMER PAD" },
+    { id: 1, name: "Ambient Pad", base: "AMBIENTE PAD" },
+    { id: 2, name: "Shimmer Pad", base: "SHIMMER PAD" },
     { id: 3, name: "Space Reverse Pad", base: "SPACE REVERSE PAD" },
-    // { id: 4, name: "Warm Pad", base: "WARM PAD" },
+    { id: 4, name: "Warm Pad", base: "WARM PAD" },
   ];
 
   const [currentNote, setCurrentNote] = useState(null);
